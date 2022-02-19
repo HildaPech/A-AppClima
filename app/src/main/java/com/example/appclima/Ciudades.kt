373653d4837c8217d61b134598069cd01878a742
HildaPech <72896837+HildaPech@users.noreply.github.com>
2022-02-19 17:38:16 -0500

    Video8_Enviando datos al intent

--- app/src/main/java/com/example/appclima/Ciudades.kt
+++ app/src/main/java/com/example/appclima/Ciudades.kt
@@ -8,6 +8,8 @@ import android.widget.Button
 import android.widget.Toast
 
 class Ciudades : AppCompatActivity() {
+    val TAG="com.example.appclima.ciudades.CIUDAD"
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_ciudades)
@@ -19,18 +21,22 @@ class Ciudades : AppCompatActivity() {
 
         bFCP.setOnClickListener(View.OnClickListener{
             val intent = Intent(this,MainActivity::class.java)
+            intent.putExtra(TAG, "Ciudad-FCP")
             startActivity(intent)
         })
         bChetumal.setOnClickListener(View.OnClickListener{
             val intent = Intent(this,MainActivity::class.java)
+            intent.putExtra(TAG, "Ciudad-Chetumal")
             startActivity(intent)
         })
         bTulum.setOnClickListener(View.OnClickListener{
             val intent = Intent(this,MainActivity::class.java)
+            intent.putExtra(TAG, "Ciudad-Tulum")
             startActivity(intent)
         })
         bCozumel.setOnClickListener(View.OnClickListener{
             val intent = Intent(this,MainActivity::class.java)
+            intent.putExtra(TAG, "Ciudad-Cozumel")
             startActivity(intent)
         })
     }
